3b71d4c40ccadb5ee133c6684da974490f669433
Roger Hutchings <roger@hutchin.gs>
2013-07-06 23:17:44 +0000

    Initial commit

---
+++ add-membership-level.php
@@ -0,0 +1,21 @@
+<?php 
+
+/*
+Plugin Name: Add Membership Level
+Description: Automatically add a WPMU Membership level to a newly-created user.
+Author: Roger Hutchings
+Version: 1.0
+Author URI: http://rogerhutchin.gs
+*/
+
+// Add action
+add_action('user_register', 'roglor_add_membership_level');
+
+
+function roglor_add_membership_level( $user_id ) {
+
+    $new_user = new M_Membership( $user_id );
+    $target_level = '2'; // Change the target level to whatever you want
+    $new_user->add_level( $target_level );
+
+}
